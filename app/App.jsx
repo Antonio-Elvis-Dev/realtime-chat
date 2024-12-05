e58b5eb0fdddb1cad6a0371d3803c3ede90073ee
Antônio Elvis <githubantonioelvis@gmail.com>
2024-12-05 00:17:51 -0300

    Adiciona navegação para novos componentes de telas
    - Cria novas telas: Friends, Profile e Requests.
    - Atualiza a navegação para incluir as telas recém-criadas.
    - Ajusta a tela Splash para exibir corretamente os links.
    - Corrige comportamentos no componente Home.
    
    Essas mudanças permitem que os usuários naveguem entre os novos componentes implementados, melhorando a experiência no app.

--- app/App.jsx
+++ app/App.jsx
@@ -1,41 +1,44 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import { StatusBar } from 'react-native';
+import {StatusBar} from 'react-native';
 
-import { SplashScreen } from './src/screens/Splash';
-import { SignInScreen } from './src/screens/SignIn';
-import { SignUpScreen } from './src/screens/SignUp';
-import { SearchScreen } from './src/screens/Search';
-import { MessageScreen } from './src/screens/Message';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen } from './src/screens/Home';
+import {SplashScreen} from './src/screens/Splash';
+import {SignInScreen} from './src/screens/SignIn';
+import {SignUpScreen} from './src/screens/SignUp';
+import {SearchScreen} from './src/screens/Search';
+import {MessageScreen} from './src/screens/Message';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {HomeScreen} from './src/screens/Home';
 
+const Stack = createNativeStackNavigator();
 
-const Stack = createNativeStackNavigator()
-
-
-export const App = ()=>{
-
-  const [initilized] = useState(true)
+export const App = () => {
+  const [initilized] = useState(true);
+  const [authenticated] = useState(true);
 
   return (
- 
     <NavigationContainer>
-      <StatusBar barStyle='dark-content' />
-      
+      <StatusBar barStyle="dark-content" />
+
       <Stack.Navigator>
-        <Stack.Screen name='Splash' component={SplashScreen}   />
-        <Stack.Screen name='SigIn' component={SignInScreen}/>
-        <Stack.Screen name='SignUp' component={SignUpScreen}/>
-        <Stack.Screen name='Home' component={HomeScreen}/>
-        <Stack.Screen name='Search' component={SearchScreen}/>
-        <Stack.Screen name='Messages' component={MessageScreen}/>
+        {!initilized ? (
+          <>
+            <Stack.Screen name="Splash" component={SplashScreen} />
+          </>
+        ) : !authenticated ? (
+          <>
+            <Stack.Screen name="SigIn" component={SignInScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Search" component={SearchScreen} />
+            <Stack.Screen name="Messages" component={MessageScreen} />
+          </>
+        )}
       </Stack.Navigator>
-
-
     </NavigationContainer>
-
   );
 };
-
--- app/src/screens/Home.jsx
+++ app/src/screens/Home.jsx
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, SafeAreaView, Text, Settings } from 'react-native';
 import React from 'react';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { RequestsScreen } from './Requests';
+import { FriendsScreen } from './Friends';
+import { ProfileScreen } from './Profile';
+
+
 export const  HomeScreen = (props)=> {
+const Tab = createBottomTabNavigator()
     return (
-        <View>
-            <Text> HomeScreen </Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name='Requests' component={RequestsScreen}/>
+            <Tab.Screen name='Friends' component={FriendsScreen}/>
+            <Tab.Screen name='Profile' component={ProfileScreen}/>
+        </Tab.Navigator>
         );
     };
